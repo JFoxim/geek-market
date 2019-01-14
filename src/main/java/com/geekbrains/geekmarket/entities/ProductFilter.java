add250b845ec4f4b225028601970473fdccff874
rinat <renanew@mail.ru>
2019-01-14 22:38:32 +0500

    +

--- src/main/java/com/geekbrains/geekmarket/entities/ProductFilter.java
+++ src/main/java/com/geekbrains/geekmarket/entities/ProductFilter.java
@@ -1,4 +1,11 @@
 package com.geekbrains.geekmarket.entities;
 
+import lombok.Data;
+
+@Data
 public class ProductFilter {
+    private String title;
+    private String categoryTitle;
+    private double priceMin;
+    private double priceMax;
 }
